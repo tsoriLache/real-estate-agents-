bf31b5d71fc71dea20e457308ef73b2674eab493
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-10 19:43:07 +0200

    started server and DB

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const url = process.env.MONGO_URI;
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('mongoDB connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
